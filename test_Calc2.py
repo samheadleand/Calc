7ed49f29946c693fc5f3edfa8846e9a373e5dd37
Sam Headleand <samheadleand@gmail.com>
2013-11-17 21:04:05 +0000

    Added tests

--- test_Calc2.py
+++ test_Calc2.py
@@ -4,6 +4,10 @@ import Calc2
 
 @pytest.mark.parametrize(('input', 'expected'), [
     ('1+1', 2),
+    ('1 + 1', 2),
+    ('2-1', 1),
+    ('2*2', 4),
+    ('4/2', 2),
 ])
 def test_calc(input, expected):
     assert Calc2.complete_sum(input) == expected
