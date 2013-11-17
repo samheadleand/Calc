532ddaad83aaa038eee98937ce3f90011e36df9c
Sam Headleand <samheadleand@gmail.com>
2013-11-17 21:16:22 +0000

    Added failing tests

--- test_Calc2.py
+++ test_Calc2.py
@@ -8,6 +8,15 @@ import Calc2
     ('2-1', 1),
     ('2*2', 4),
     ('4/2', 2),
+    ('2+5+3', 10), # 1 2
+    ('(1+2)*3', 9), # 4 4
+    ('5+7-2', 10), # 2 3
+    ('6+2=', 8), # 3 1
+    ('1+2*3', 7), # 5 5
 ])
+
+# Ordering: Importance (1 Important, 5 Unimportant)
+#           Difficulty (1 Easy, 5 Difficult)
+
 def test_calc(input, expected):
     assert Calc2.complete_sum(input) == expected
